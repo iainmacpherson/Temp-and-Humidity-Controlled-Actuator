18aa25eeead09c2d2863e63c17c4fd9cd05f0dcd
Iain Macpherson <Iain.Macpherson@cirrus.com>
2022-08-29 17:01:31 +0100

    add code for actuation

--- src/feedback_actuator.ino
+++ src/feedback_actuator.ino
@@ -15,7 +15,7 @@
  *=============================================================================*/
 // Print data to the serial consol
 void serialLogData(float h, float t) {
-  Serial.print(F(" Humidity: "));
+  Serial.print(F("[SENSOR]: Humidity: "));
   Serial.print(h);
   Serial.print(F("%  Temperature: "));
   Serial.print(t);
@@ -37,16 +37,41 @@ SensorData getSensorData(DHT dht) {
   float t = dht.readTemperature(); //reads temperature
 
   if (isnan(h) || isnan(t)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
+    Serial.println(F("[SENSOR]: Failed to read from DHT sensor!"));
     return null;
   }
   return result;
 }
 
 
-int computeAdjustment(SensorData d) { return 0; }
+int computeAdjustment(SensorData d) {
+  // TODO: actually implement this
+  // testing code
+  static int adjustment = -1000;
+  adjustment = -adjustment;
+  return adjustment;
+}
 
-void makeAdjustment(int adjustment) { }
+void makeAdjustment(int adjustment) {
+  if (adjustment < 0) {
+    // retract the actuator for that number of miliseconds
+    digitalWrite(relay_1, LOW);
+    digitalWrite(relay_2, HIGH);
+    delay(adjustment);
+    digitalWrite(relay_1, LOW);
+    digitalWrite(relay_2, LOW);
+  } else if (adjustment > 0) {
+    // extend the actuator for that number of miliseconds
+    digitalWrite(relay_1, HIGH);
+    digitalWrite(relay_2, LOW);
+    delay(adjustment);
+    digitalWrite(relay_1, LOW);
+    digitalWrite(relay_2, LOW);
+  } else {
+    // do nothing
+    return;
+  }
+}
 
 /*=============================================================================
     Startup Code
@@ -83,18 +108,22 @@ void setup() {
     (maybe) adjust update_interval
 */
 
+/* Tuning Parameters:
+    step magnitude - how much to move per step?
+    update_interval - how frequently is the sensor checked and updated.
+*/
 void loop() {
   int update_interval = 500; // the time between updates in ms
   // read new data from the sensor
   SensorData data = getSensorData(dht);
   if (data == null) return
+  // print data to the console
+  serialLogData(data.humidity, data.temperature);
 
   int adjustment = computeAdjustment(data);
   makeAdjustment(adjustment);
 
-  // print data to the console
-  serialLogData(data.humidity, data.temperature);
-
   // wait for some amount of time in miliseconds
   delay(update_interval);
 }
+
